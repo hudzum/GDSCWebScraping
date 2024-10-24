fa24811551c02669dd20c7a4f038481396b16b0f
hudzum <83727915+hudzum@users.noreply.github.com>
2024-10-24 18:38:56 -0500

    Update First_spider.py Comments and stuff

--- First_spider.py
+++ First_spider.py
@@ -1,12 +1,12 @@
 import scrapy
 
 class BookScraper(scrapy.Spider):
-    name = "book" 
+    name = "book"  #This must be unique among 
     start_urls = ["https://books.toscrape.com/"] 
 
     def parse(self, response): 
            
             yield {
-                'title': response.css('.h1 a::text').get() 
+                'title': response.css('.h1 a::text').get() #CSS Selector for grabing the title!
             }
 
